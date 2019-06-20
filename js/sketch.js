b654deeb6acc0f8385a49ea8a67682769e2ea783
Josh Newton <josh.newton@me.com>
2019-06-20 15:52:42 -0400

    Fixing audio location issues

--- js/sketch.js
+++ js/sketch.js
@@ -17,7 +17,7 @@ function preload() {
     // For local testing
     vinyl = loadSound('../audio/Vinyl.mp3',
                       function(){console.log("File loaded.");},
-                      function(){vinyl = loadSound(getURL() + getURLpath() + '/audio/Vinyl.mp3');});
+                      function(){vinyl = loadSound(getURL() + getURLPath() + '/audio/Vinyl.mp3');});
   }
   catch(error) {
     console.error(error);
