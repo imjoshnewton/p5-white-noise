ffc7c464ffb8378335225d0f34ba3ec3271d5549
Josh Newton <josh.newton@me.com>
2019-06-20 16:15:17 -0400

    Fixing audio path issues

--- js/sketch.js
+++ js/sketch.js
@@ -13,18 +13,15 @@ var noiseFFT,
   played = false;
 
 function preload() {
-    try {
     // For local testing
     vinyl = loadSound('../audio/Vinyl.mp3',
                       function(){
                         console.log("File loaded.");
+                      },
+                      function(){
+                        console.log("File not loaded by reference, loading by path:" + getURL());
+                        vinyl = loadSound(getURL() + "/audio/Vinyl.mp3");
                       });
-    }
-    catch(error) {
-      console.log(error);
-      console.log("File not loaded by reference, loading by path:" + getURL());
-      vinyl = loadSound(getURL() + "/audio/Vinyl.mp3");
-    }
 }
 
 function setup() {
